43b32b4f2b85a7dd6f678db563a787e498dc9d38
Rich14re <84147059+Rich14re@users.noreply.github.com>
2024-09-26 00:15:53 +0300

    Classes Of Figures

--- obj/Debug/net5.0-windows/MiniPaint.AssemblyInfo.cs
+++ obj/Debug/net5.0-windows/MiniPaint.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniPaint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+705debf2345b616d2b6a2ea127871845ae012e95")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniPaint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniPaint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
